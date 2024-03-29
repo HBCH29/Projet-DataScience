37db7ebf03ffdb908a48582b62ee5d7c1bf525d1
0x2A-git <lucas.martin0@protonmail.com>
2024-03-30 00:05:09 +0100

    feat(data): add plotting

--- tache1.py
+++ tache1.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import dates as mdates
 import os
 
 folder = 'TP Data 2024 - FISA'
@@ -9,7 +11,9 @@ def chercher_fichiers_identiques(input_folder):
     fichiers_identiques = {
             'MOD1.txt': [],
             'MOD2.txt': [],
-            'POD.csv': [],
+            'POD 200085.csv': [],
+            'POD 200086.csv': [],
+            'POD 200088.csv': [],
             'PICO.csv': [],
             'THICK.csv': [],
             'THIN.csv': [],
@@ -26,8 +30,12 @@ def chercher_fichiers_identiques(input_folder):
                 fichiers_identiques["MOD1.txt"].append(file_path)
             if "MOD2" in file_name_upper:
                 fichiers_identiques["MOD2.txt"].append(file_path)
-            elif "POD" in file_name_upper:
-                fichiers_identiques["POD.csv"].append(file_path)
+            elif "POD 200085" in file_name_upper:
+                fichiers_identiques["POD 200085.csv"].append(file_path)
+            elif "POD 200086" in file_name_upper:
+                fichiers_identiques["POD 200086.csv"].append(file_path)
+            elif "POD 200088" in file_name_upper:
+                fichiers_identiques["POD 200088.csv"].append(file_path)
             elif "PICO" in file_name_upper:
                 fichiers_identiques["PICO.csv"].append(file_path)
             elif "THICK" in file_name_upper:
@@ -122,7 +130,7 @@ def cleanup_csv_files(filename:str, files_paths):
     filename = filename.upper()
     df_full = pd.DataFrame()
     for csv in files_paths:
-        newDf = pd.read_csv(csv,comment="#", sep=";")
+        newDf = pd.read_csv(csv,comment="#", sep=";", skiprows=(1,2,3,4))
         df_full = pd.concat([ df_full, newDf ])
 
     # Then cleanup junk
@@ -159,64 +167,103 @@ def cleanup_by_files_type(filename ,files_paths):
 
     return df
 
-# def cleanup_file(file_path):
-
-
-
 # Main function to clean files
 def cleanup_files(input_files):
 
+    dfs = dict()
     print("Scanning for files to cleanup...")
     # Traiter les fichiers identiques
     for nom_fichier, chemins in input_files.items():
-
+        filename, ext = os.path.splitext( nom_fichier)
         print(f"{'-' * 10} {nom_fichier} {'-' * 10}")
         if len(chemins) > 1 :
-            output_file_path = os.path.join(output, nom_fichier)
+            output_file_path = os.path.join(output, f"{filename}.csv")
                # Vérifier si le fichier de sortie existe déjà
             if os.path.exists(output_file_path):
                 print(f"Le fichier '{output_file_path}' existe déjà.")
+                # TODO : debug matplotlib hanging
+                # print("Saving df to memory...")
+                # df = pd.read_csv(output_file_path,sep=",")
+                # dfs[filename.lower()] = df
+                # print("Done saving df to memory !")
             else:
                 df = cleanup_by_files_type(nom_fichier, chemins)
-                print("Saving file...")
+                print("Saving df to memory...")
+
+                dfs[filename.lower()] = df
+
+                print("Done saving df to memory !")
+
+                print("Saving df to file...")
+
                 df.to_csv(output_file_path, index=False)
 
                 print("Done saving file !")
         print("-" * 20)
 
     print("Done cleaning up files !")
-        # else:
-        #     print(f"Traitement des fichiers pour '{nom_fichier}':")
-        #     dfs = [] 
-        #     if nom_fichier.endswith(".csv"):
-        #         for chemin in chemins:
-        #             df = pd.read_csv(chemin,comment="#")
-        #             dfs.append(df)
-        #         concatenated_df = pd.concat(dfs, ignore_index=True)
-        #         print(concatenated_df.info())
-        #         try:
-        #             # Vérifier les échantillons en double
-        #             duplicates = concatenated_df.duplicated()
-
-        #             # Afficher les échantillons en double
-        #             print("Échantillons en double :")
-        #             print(concatenated_df[duplicates])
-
-        #             # Compter le nombre d'échantillons en double
-        #             nombre_doublons = duplicates.sum()
-        #             print(f"Nombre total d'échantillons en double : {nombre_doublons}")
-        #             # Supprimer les échantillons en double
-        #             concatenated_df.drop_duplicates(inplace=True)
-        #             print("Echantillons en double ont été supprimé")
-        #             # Trier par date
-        #             concatenated_df['date'] = pd.to_datetime(concatenated_df['date'])
-        #             concatenated_df = concatenated_df.sort_values(by='date')
-        #             print("trié :D ")
-        #         except Exception as e :
-        #             print(f"Error has occured {e}")
-        #         # Enregistrer le DataFrame résultant dans un nouveau fichier CSV
-        #         concatenated_df.to_csv(output_file_path, index=False)
-        #         print(f"Fichier '{output_file_path}' créé avec succès.")
+    return dfs
 
 files_to_clean = chercher_fichiers_identiques(folder)
-cleanup_files(files_to_clean)
+files_clean = cleanup_files(files_to_clean)
+print(files_clean)
+plt.figure(figsize=(10, 2))  # Plot overview of the files
+date_format = mdates.DateFormatter('%d-%b')
+
+plt.subplot(3,2,1) # MOD1
+plt.subplots_adjust(top=8)
+plt.title("MOD1 (Temperature x Time)")
+print("Plotting mod1...")
+plt.plot(files_clean['mod1']['Time'],files_clean['mod1']['Temperature'])
+plt.gca().xaxis.set_major_formatter(date_format)
+print("Done Plotting mod1 !")
+
+plt.xticks(rotation=45)
+
+plt.subplot(3,2,2) # MOD2
+plt.subplots_adjust(top=8)
+plt.title("MOD2 (Temperature x Time)")
+
+print("Plotting mod2...")
+plt.plot(files_clean['mod2']['Time'],files_clean['mod2']['Temperature'])
+plt.gca().xaxis.set_major_formatter(date_format)
+plt.xticks(rotation=45)
+print("Done plotting mod2 !")
+
+plt.subplot(3,2,3) # POD 200085
+plt.subplots_adjust(top=8)
+plt.title("POD 200085 (Temperature x Time)")
+print("Plotting pod 200085...")
+plt.plot(files_clean['pod 200085']['Time'],files_clean['pod 200085']['temperature'])
+plt.gca().xaxis.set_major_formatter(date_format)
+plt.xticks(rotation=45)
+print("Done plotting pod 200085 !")
+
+plt.subplot(3,2,4) # Piano PICO
+plt.subplots_adjust(top=8)
+plt.title("PICO (Temperature x Time)")
+print("Plotting pico...")
+plt.plot(files_clean['pico']['Time'],files_clean['pico']['bme68x_temp'])
+plt.gca().xaxis.set_major_formatter(date_format)
+plt.xticks(rotation=45)
+print("Done plotting pico !")
+
+plt.subplot(3,2,5) # Piano Thick
+plt.subplots_adjust(top=8)
+plt.title("THICK (TGS2620 x Time)")
+plt.plot(files_clean['thick']['Time'], files_clean['thick']['piano_TGS2620I00'])
+plt.gca().xaxis.set_major_formatter(date_format)
+plt.xticks(rotation=45)
+
+plt.subplot(3,2,6) # Piano Thin
+plt.subplots_adjust(top=8)
+plt.title("THIN (GM102B x Time)")
+plt.plot(files_clean['thin']['Time'],files_clean['thin']['piano_GM102BI00'])
+plt.gca().xaxis.set_major_formatter(date_format)
+plt.xticks(rotation=45)
+
+plt.subplots_adjust(left=0.1, bottom=0.06, right=0.9, top=0.96, wspace=0.2, hspace=0.2)
+plt.savefig("plot.png")
+plt.show()
+
+
