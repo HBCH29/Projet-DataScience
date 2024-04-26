b83bc5b3f75aaa3875264531ccd86e73f19257c6
0x2A-git <lucas.martin0@protonmail.com>
2024-04-26 23:15:52 +0200

    feat(ml): remove useless features

--- seance3.py
+++ seance3.py
@@ -6,8 +6,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.utils import shuffle
+from sklearn.feature_selection import SelectKBest, f_classif
 from scipy.sparse import coo_matrix
 import numpy as np
+
 
 # Path to your custom CSV dataset
 csv_file = "tache3/labels.csv"
@@ -21,36 +23,53 @@ X = data.drop('label', axis=1)  # Features
 
 y = data['label']  # Labels
 
+k = 53
+print(f"K: {k}")
+# Feature selection
+selector = SelectKBest(score_func=f_classif, k=k)  # Find k features to drop
+X_selected = selector.fit_transform(X, y)
+mask = selector.get_support()
 
-X_sparse = coo_matrix(X)
+dropped_features = X.columns[~mask]
+print(f"Dropped columns {dropped_features}")
 
-X, X_sparse, y = shuffle(X, X_sparse, y)
+# Shuffle and split data
+X_selected_sparse = coo_matrix(X_selected)
+X_selected, X_selected_sparse, y = shuffle(X_selected, X_selected_sparse, y)
+X_train_selected, X_test_selected, y_train_selected, y_test_selected = train_test_split(X_selected, y, test_size=0.2)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-
+# Standardize features
 scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+X_train_selected = scaler.fit_transform(X_train_selected)
+X_test_selected = scaler.transform(X_test_selected)
 
+# Classifier
 knn = KNeighborsClassifier(n_neighbors=4)  # You can adjust the number of neighbors as needed
-knn.fit(X_train, y_train)
+knn.fit(X_train_selected, y_train_selected)
 
-y_pred = knn.predict(X_test)
+# Prediction
+y_pred_selected = knn.predict(X_test_selected)
 
-print("Accuracy:", accuracy_score(y_test, y_pred))
-print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+# Evaluation
+print("Accuracy:", accuracy_score(y_test_selected, y_pred_selected))
+print("Confusion Matrix:\n", confusion_matrix(y_test_selected, y_pred_selected))
 
-scores = cross_validate(knn, X, y, cv=5, scoring='f1_macro', return_train_score=True)
+# Cross-validation
+scores_selected = cross_validate(knn, X_selected, y, cv=5, scoring='f1_macro', return_train_score=True)
 
-print("Training Scores:", scores['train_score'])
-print("Testing Scores:", scores['test_score'])
+print("Training Scores:", scores_selected['train_score'])
+print("Testing Scores:", scores_selected['test_score'])
 
-print("Cross-Validation Scores:", scores)
-print(f"Mean F1 : { np.mean(scores['test_score']) }")
+print("Cross-Validation Scores:", scores_selected)
+print(f"Average F1 : {np.average(scores_selected['test_score'])}")
+print("-------------------")
 
-# print("-------------------")
+# Uncomment if you want to test model's predictions
 
-# data_scaled = scaler.transform(data.drop('label', axis=1))
-# p = knn.predict([ data_scaled[0] ])
+# test_dataset = data.drop([*dropped_features,'label'], axis=1)
 
+# data_scaled = scaler.transform(test_dataset)
+# p = knn.predict(data_scaled)
+
+# for i in range(data.shape[0]):
+#     print(f"{data.iloc[i]['label']} -> {p[i]}")
