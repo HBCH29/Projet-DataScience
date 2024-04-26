d511a1419ff5c98703a2cf389ecd3f8c99543395
0x2A-git <lucas.martin0@protonmail.com>
2024-04-26 21:54:12 +0200

    chore: remove wip

--- seance3.py
+++ seance3.py
@@ -7,6 +7,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.utils import shuffle
 from scipy.sparse import coo_matrix
+import numpy as np
 
 # Path to your custom CSV dataset
 csv_file = "tache3/labels.csv"
@@ -17,6 +18,7 @@ data = pd.read_csv(csv_file, sep=";")
 data = data.dropna()
 
 X = data.drop('label', axis=1)  # Features
+
 y = data['label']  # Labels
 
 
@@ -31,8 +33,7 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-
-knn = KNeighborsClassifier(n_neighbors=5)  # You can adjust the number of neighbors as needed
+knn = KNeighborsClassifier(n_neighbors=4)  # You can adjust the number of neighbors as needed
 knn.fit(X_train, y_train)
 
 y_pred = knn.predict(X_test)
@@ -46,41 +47,10 @@ print("Training Scores:", scores['train_score'])
 print("Testing Scores:", scores['test_score'])
 
 print("Cross-Validation Scores:", scores)
+print(f"Mean F1 : { np.mean(scores['test_score']) }")
 
+# print("-------------------")
 
-print("-------------------")
-
-data_scaled = scaler.transform(data.drop('label', axis=1))
-p = knn.predict(data_scaled)
-
-# for i in range(data.shape[0]):
-#     print(f"{data.iloc[i]['label']} -> {p[i]}")
-
-
-# WIP
-# Calculer la matrice de corrélation
-# corr_matrix = data.corr().corr(method='pearson')
-# Définir un seuil de corrélation élevée
-# threshold = 0.2
-
-# Parcourir les colonnes et supprimer les colonnes redondantes
-# redundant_columns = []
-
-# print(corr_matrix)
-    # for j in range(i+1, len(corr_matrix.columns)):
-
-    #     if abs(corr_matrix.iloc[i, j]) <= threshold :
-    #         print(abs(corr_matrix.iloc[i, j]))
-    #         redundant_columns.append(corr_matrix.columns[j])
-
-# Supprimer les colonnes redondantes du jeu de données
-# data = data.drop(redundant_columns, axis=1)
-# import seaborn as sns
-# fig, ax = plt.subplots()
-# sns.heatmap(data.corr(), annot=True)
-# plt.show()
-
-# Create an instance of your custom dataset
-# custom_dataset = CustomDataset(csv_file)
- 
+# data_scaled = scaler.transform(data.drop('label', axis=1))
+# p = knn.predict([ data_scaled[0] ])
 
