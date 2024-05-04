f1df203ca724bf6b9ae32dc9a89dd4febb77fd1e
Lucas Martin <20931885+0x2A-git@users.noreply.github.com>
2024-05-04 14:28:56 +0200

    feat(ml): add knn (#5)
    * feat(ml): add knn model
    
    * chore: remove wip
    
    * feat(ml): remove useless features
    
    * refactor: remove selected from vars names
    
    * refactor: add logs

--- seance3.py
+++ seance3.py
@@ -1,19 +1,15 @@
 import pandas as pd
-import torch
-from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt
-# Define your custom dataset class
-class CustomDataset(Dataset):
-    def __init__(self, csv_file):
-        self.data = pd.read_csv(csv_file, sep=";")  # Read CSV file into DataFrame
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import cross_validate, train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.utils import shuffle
+from sklearn.feature_selection import SelectKBest, f_classif
+from scipy.sparse import coo_matrix
+import numpy as np
 
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        sample = self.data.iloc[idx, :]  # Get row at index 'idx'
-        # sample = torch.tensor(sample.values.astype(float))
-        return sample
 
 # Path to your custom CSV dataset
 csv_file = "tache3/labels.csv"
@@ -21,31 +17,62 @@ csv_file = "tache3/labels.csv"
 # Charger les données à partir d'un fichier CSV
 data = pd.read_csv(csv_file, sep=";")
 
-print(data.columns.shape)
-# Calculer la matrice de corrélation
-corr_matrix = data.corr().corr(method='pearson')
-# Définir un seuil de corrélation élevée
-threshold = 0.7
+data = data.dropna()
 
-# Parcourir les colonnes et supprimer les colonnes redondantes
-redundant_columns = []
-for i in range(len(corr_matrix.columns)):
-    for j in range(i+1, len(corr_matrix.columns)):
-        if abs(corr_matrix.iloc[i, j]) >= threshold :
-            print(abs(corr_matrix.iloc[i, j]))
-            redundant_columns.append(corr_matrix.columns[j])
+X = data.drop('label', axis=1)  # Features
 
-# Supprimer les colonnes redondantes du jeu de données
-data = data.drop(redundant_columns, axis=1)
-import seaborn as sns
-fig, ax = plt.subplots()
-sns.heatmap(data.corr(), annot=True)
-plt.show()
-# Create an instance of your custom dataset
-custom_dataset = CustomDataset(csv_file)
+y = data['label']  # Labels
 
+k = 53
+print(f"K: {k}")
+# Feature selection
+selector = SelectKBest(score_func=f_classif, k=k)  # Find k features to drop
+X_selected = selector.fit_transform(X, y)
+mask = selector.get_support()
 
-# Create a dataloader for your data
-train_loader = DataLoader(dataset=custom_dataset, batch_size=50, shuffle=True)
+dropped_features = X.columns[~mask]
+print(f"Dropped columns {dropped_features}")
 
-print(train_loader.dataset[0]['label'])
+# Shuffle and split data
+X_selected_sparse = coo_matrix(X_selected)
+X_selected, X_selected_sparse, y = shuffle(X_selected, X_selected_sparse, y)
+X_train, X_test, y_train_selected, y_test_selected = train_test_split(X_selected, y, test_size=0.2)
+
+# Standardize features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Classifier
+knn = KNeighborsClassifier(n_neighbors=4)  # You can adjust the number of neighbors as needed
+knn.fit(X_train, y_train_selected)
+
+# Prediction
+y_pred = knn.predict(X_test)
+
+# Evaluation
+print("Accuracy:", accuracy_score(y_test_selected, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test_selected, y_pred))
+
+# Cross-validation
+
+print("Cross validating...")
+scores = cross_validate(knn, X_selected, y, cv=5, scoring='f1_macro', return_train_score=True)
+
+print("Done cross validating !")
+print("Training Scores:", scores['train_score'])
+print("Testing Scores:", scores['test_score'])
+
+print("Cross-Validation Scores:", scores)
+print(f"Average F1 : {np.average(scores['test_score'])}")
+print("-------------------")
+
+# Uncomment if you want to test model's predictions
+
+# test_dataset = data.drop([*dropped_features,'label'], axis=1)
+
+# data_scaled = scaler.transform(test_dataset)
+# p = knn.predict(data_scaled)
+
+# for i in range(data.shape[0]):
+#     print(f"{data.iloc[i]['label']} -> {p[i]}")
